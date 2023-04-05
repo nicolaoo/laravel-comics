c4927c0b361a089693df933456f87a2bdd6fc313
nicola00 <nicolajobs00@gmail.com>
2023-04-05 17:09:32 +0200

    FEAT main add section and first div complete

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,4 +1,33 @@
 <main>
-    <h1>main</h1>
-
+    <section>
+        <div>
+            <figure>
+                <img class="jumbo-img" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="">
+            </figure>
+        </div>
+        <div>
+            <div class="gird">
+                <div class="card">
+                    <img src="" alt="">
+                    <p>action comics</p>
+                </div>
+                <div class="card">
+                    <img src="" alt="">
+                    <p>action comics</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <img src="" alt="">
+                    <p>digital comics</p>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </section>
 </main>
