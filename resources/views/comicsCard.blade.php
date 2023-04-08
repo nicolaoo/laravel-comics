b0ed574c7dada8f2d884c89fb7e00199b22cede3
nicola00 <nicolajobs00@gmail.com>
2023-04-08 10:04:49 +0200

    FEAT headerCard

--- resources/views/comicsCard.blade.php
+++ resources/views/comicsCard.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/5ffb897fe9.js" crossorigin="anonymous"></script>
     <title>@yield('page-title')</title>
 
     <!-- Styles -->
--- resources/views/partials/headerCard.blade.php
+++ resources/views/partials/headerCard.blade.php
@@ -1,5 +1,35 @@
 <header>
-    <h1>
-        card header
-    </h1>
+    <div class="row-up-head">
+        <div class="container">
+            <ul>
+                <li>
+                    dc power visa
+                    <i class="fa-regular fa-registered"></i>
+                </li>
+                <li>
+                    additional dc sites
+                    <i class="fa-solid fa-sort-down"></i>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row-head">
+            <figure>
+                <img src="{{ Vite::asset('resources/images/favicon.ico') }}" alt="">
+            </figure>
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+    </div>
 </header>
